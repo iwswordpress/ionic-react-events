ab94792ff8f6997763380e99a803f860bef5bae9
iwswordpress <iwswordpress@gmail.com>
2020-11-18 19:05:55 +0000

    56

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import {
-  IonApp,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
-  IonIcon
-} from '@ionic/react';
+import { IonApp, IonRouterOutlet } from '@ionic/react';
 import React, { useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { IonReactRouter } from '@ionic/react-router';
-import { home as homeIcon, settings as settingsIcon } from 'ionicons/icons';
-import HomePage from './pages/HomePage';
-import SettingsPage from './pages/SettingsPage';
-import EntryPage from './pages/EntryPage';
+
 import LoginPage from './pages/LoginPage';
 import AppTabs from './AppTabs';
 const App: React.FC = () => {
