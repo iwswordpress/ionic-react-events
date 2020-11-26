651bf18e36be628075158f5286ccc974637d23b6
iwswordpress <iwswordpress@gmail.com>
2020-11-26 09:41:24 +0000

    73 plus new WebComponents.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,46 +10,8 @@ import {
 import React, { useEffect, useState } from 'react';
 import { firestore } from '../firebase';
 import './HomePage.css';
-import { Entry, toEntry } from '../models';
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'uc-stock-finder': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-    interface IntrinsicElements {
-      'my-component': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-    interface IntrinsicElements {
-      'iws-test': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-    interface IntrinsicElements {
-      'app-home': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-    interface MyElementAttributes {
-      'first-name': string;
-      'middle-name': string;
-      'last-name': string;
-    }
-    interface IntrinsicElements {
-      'uc-stock-price': React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      >;
-    }
-  }
-}
+import { toEntry } from '../models';
+
 const HomePage: React.FC = () => {
   const [entries, setEntries] = useState<any[]>([]);
 
@@ -88,17 +50,6 @@ const HomePage: React.FC = () => {
             </IonItem>
           ))}
         </IonList>
-        <div style={{ marginTop: '30px', color: '#2196f3' }}>
-          <my-component
-            first-name='Craig'
-            middle-name='George'
-            last-name='West'
-          ></my-component>
-        </div>
-        <div style={{ maxWidth: '350px', width: '100%', marginLeft: '-30px' }}>
-          <uc-stock-price stock-symbol='AAPL'></uc-stock-price>
-          <uc-stock-finder></uc-stock-finder>
-        </div>
       </IonContent>
     </IonPage>
   );
