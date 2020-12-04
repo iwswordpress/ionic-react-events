cc3657134a95cafbffee3f62549e2f75af0f5db0
iwswordpress <iwswordpress@gmail.com>
2020-12-04 20:07:44 +0000

    80

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -1,14 +1,17 @@
 import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
   IonContent,
   IonHeader,
+  IonIcon,
   IonPage,
   IonTitle,
-  IonToolbar,
-  IonButtons,
-  IonBackButton
+  IonToolbar
 } from '@ionic/react';
+import { trash as trashIcon } from 'ionicons/icons';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import './EntryPage.css';
 import { firestore } from '../firebase';
 import { Entry, toEntry, RouteParams } from '../models';
@@ -19,6 +22,7 @@ const EntryPage: React.FC = () => {
   const { userId } = useAuth();
   const { id } = useParams<RouteParams>();
   const [entry, setEntry] = useState<Entry>();
+  const history = useHistory();
 
   useEffect(() => {
     //const entryRef = firestore.collection('entries').doc(id);
@@ -34,12 +38,27 @@ const EntryPage: React.FC = () => {
     });
   }, [id]);
 
+  const handleDelete = async () => {
+    const entryRef = firestore
+      .collection('users')
+      .doc(userId)
+      .collection('entries')
+      .doc(id);
+    await entryRef.delete();
+    console.log(`Item ${id} ${entry.title} DELETED`);
+    history.goBack();
+  };
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot='start'>
             <IonBackButton> Back</IonBackButton>
+          </IonButtons>
+          <IonButtons slot='end'>
+            <IonButton onClick={handleDelete}>
+              <IonIcon icon={trashIcon} slot='icon-only' />
+            </IonButton>
           </IonButtons>
           <IonTitle>
             {entry?.id}:{entry?.title}
